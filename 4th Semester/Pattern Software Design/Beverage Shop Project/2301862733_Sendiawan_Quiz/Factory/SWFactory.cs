5fba76e6ae6e28ea19c46e8a4aba8a1713819949
Sendiawan Muljono <87474300+SendiawanMuljono@users.noreply.github.com>
2021-07-16 01:06:52 +0700

    Add files via upload

---
+++ 4th Semester/Pattern Software Design/Beverage Shop Project/2301862733_Sendiawan_Quiz/Factory/SWFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using _2301862733_Sendiawan_Quiz.Model;
+
+namespace _2301862733_Sendiawan_Quiz.Factory
+{
+    public class SWFactory
+    {
+        public static Beverage CreateBeverage(string name, string type, string description, int price)
+        {
+            Beverage beverage = new Beverage();
+            beverage.Name = name;
+            beverage.Type = type;
+            beverage.Description = description;
+            beverage.Price = price;
+            return beverage;
+        }
+
+        public static Beverage CreateBeverage(int id, string name, string type, string description, int price)
+        {
+            Beverage beverage = new Beverage();
+            beverage.Id = id;
+            beverage.Name = name;
+            beverage.Type = type;
+            beverage.Description = description;
+            beverage.Price = price;
+            return beverage;
+        }
+    }
+}
